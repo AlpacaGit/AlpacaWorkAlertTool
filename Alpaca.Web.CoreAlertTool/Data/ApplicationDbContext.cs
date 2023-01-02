22e7b4233d188ded2c3c18dd1785c72a61df1ad9
Alpaca <sushingoro@gmail.com>
2023-01-02 22:46:57 +0900

    ほぼ手つかずのASP .NET MVC (Core)のプロジェクトをPush

---
+++ Alpaca.Web.CoreAlertTool/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alpaca.Web.CoreAlertTool.Data
+{
+    /// <summary>
+    /// DBContextクラス。
+    /// </summary>
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Alpaca.Web.CoreAlertTool/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Alpaca.Web.CoreAlertTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
